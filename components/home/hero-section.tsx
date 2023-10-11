1dd5b432dfa3380ffc8b0b29f2cad6f3178584d7
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-11 20:42:47 +0300

    naichapa an finyo

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -6,31 +6,12 @@ const HeroArea = () => {
       {/* Header Section */}
       <div className="hero_bg_box">
         <div className="img-box">
-          <img src="/images/hero-bg.jpg" alt="" />
+          <img src="../../public/images/about-img.jpg" alt="" />
         </div>
       </div>
 
       <header id="home" className="header_section">
-        {/* Header Top */}
-        <div className="header_top">
-          <div className="container-fluid">
-            <div className="contact_link-container">
-              <a href="" className="contact_link1">
-                <i className="fa fa-map-marker" aria-hidden="true"></i>
-                <span className="pl-2">Nairobi, Kenya</span>
-              </a>
-              <a href="" className="contact_link2">
-                <i className="fa fa-phone" aria-hidden="true"></i>
-                <span>Call: (+254) 719 230590</span>
-              </a>
-              <a href="mailto:sledgeentertainment8@gmail.com" className="contact_link3">
-                <i className="fa fa-envelope" aria-hidden="true"></i>
-                <span>sledgeentertainment8@gmail.com</span>
-              </a>
-            </div>
-          </div>
-        </div>
-
+    
         {/* Header Bottom */}
         <div className="header_bottom">
           <div className="container-fluid">
