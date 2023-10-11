48c9cdcf4552f5c9b5d669f0638a863f594f4aa2
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-11 20:34:55 +0300

    usipack gari

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,153 +1,116 @@
-<div class="hero_area">
-<!-- header section strats -->
-<div class="hero_bg_box">
-  <div class="img-box">
-    <img src="images/hero-bg.jpg" alt="">
-  </div>
-</div>
+import React from 'react';
 
-<header id="home" class="header_section">
-  <div class="header_top">
-    <div class="container-fluid">
-      <div class="contact_link-container">
-        <a href="" class="contact_link1">
-          <i class="fa fa-map-marker" aria-hidden="true"></i>
-          <span class="pl-2">
-            Nairobi, Kenya </span>
-        </a>
-        <a href="" class="contact_link2">
-          <i class="fa fa-phone" aria-hidden="true"></i>
-          <span>
-            Call : (+254) 719 230590
-          </span>
-        </a>
-        <a href="mailto:sledgeentertainment8@gmail.com" class="contact_link3">
-          <i class="fa fa-envelope" aria-hidden="true"></i>
-          <span>sledgeentertainment8@gmail.com
-          </span>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="header_bottom">
-    <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg custom_nav-container">
-        <a class="navbar-brand" href="#home">
-          <div class="image-box">
-            <img src="images/log.png" alt="" style="max-height:75px;">
-          </div>
-
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""></span>
-        </button>
-
-        <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
-          <ul class="navbar-nav  ">
-            <li class="nav-item active">
-              <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about"> About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#services"> Services </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#projects"> Projects </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact">Contact us</a>
-            </li>
-          </ul>
+const HeroArea = () => {
+  return (
+    <div className="hero_area">
+      {/* Header Section */}
+      <div className="hero_bg_box">
+        <div className="img-box">
+          <img src="/images/hero-bg.jpg" alt="" />
         </div>
-      </nav>
-    </div>
-  </div>
-</header>
-<!-- end header section -->
-<!-- slider section -->
-<section class=" slider_section ">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7">
-              <div class="detail-box">
-                <h1>
-                  Crafting Cinematic<br>
-                  <span>
-                    experiences </span>
-                </h1>
-                <p>
-                  Transforming ideas into captivating stories through the magic of film. Experience creativity in
-                  motion.
-                </p>
-                <div class="btn-box">
-                  <a href="#about" class="btn-1"> Learn more </a>
-                  <a href="#contact" class="btn-2">Get A Quote</a>
-                </div>
-              </div>
+      </div>
+
+      <header id="home" className="header_section">
+        {/* Header Top */}
+        <div className="header_top">
+          <div className="container-fluid">
+            <div className="contact_link-container">
+              <a href="" className="contact_link1">
+                <i className="fa fa-map-marker" aria-hidden="true"></i>
+                <span className="pl-2">Nairobi, Kenya</span>
+              </a>
+              <a href="" className="contact_link2">
+                <i className="fa fa-phone" aria-hidden="true"></i>
+                <span>Call: (+254) 719 230590</span>
+              </a>
+              <a href="mailto:sledgeentertainment8@gmail.com" className="contact_link3">
+                <i className="fa fa-envelope" aria-hidden="true"></i>
+                <span>sledgeentertainment8@gmail.com</span>
+              </a>
             </div>
           </div>
         </div>
-      </div>
-      <div class="carousel-item ">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7">
-              <div class="detail-box">
-                <h1>
-                  Elevating visual<br>
-                  <span>
-                    storytelling </span>
-                </h1>
-                <p>
-                  Concepts become vivid realities, leaving an indelible mark on audiences. See your vision come
-                  alive.
-                </p>
-                <div class="btn-box">
-                  <a href="#about" class="btn-1"> Learn more </a>
-                  <a href="#contact" class="btn-2">Get A Quote</a>
+
+        {/* Header Bottom */}
+        <div className="header_bottom">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg custom_nav-container">
+              <a className="navbar-brand" href="#home">1
+                <div className="image-box">
+                  <img src="/images/log.png" alt="" style={{ maxHeight: '75px' }} />
+                </div>
+              </a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className=""></span>
+              </button>
+
+              <div className="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#home">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#about">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#services">Services</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#projects">Projects</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#contact">Contact us</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      {/* Slider Section */}
+      <section className="slider_section">
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            {/* Carousel Items */}
+            <div className="carousel-item active">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-7">
+                    <div className="detail-box">
+                      <h1>
+                        Crafting Cinematic<br />
+                        <span>experiences</span>
+                      </h1>
+                      <p>
+                        Transforming ideas into captivating stories through the magic of film. Experience creativity in
+                        motion.
+                      </p>
+                      <div className="btn-box">
+                        <a href="#about" className="btn-1">Learn more</a>
+                        <a href="#contact" className="btn-2">Get A Quote</a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
+            {/* Additional Carousel Items */}
+            {/* Add your additional carousel items here */}
+          </div>
+          <div className="container indicator_container">
+            <ol className="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
           </div>
         </div>
-      </div>
-      <div class="carousel-item ">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7">
-              <div class="detail-box">
-                <h1>
-                  Unleash your<br>
-                  <span>
-                    creative vision </span>
-                </h1>
-                <p>
-                  Collaborate to bring your ideas to life with precision, creativity, and innovation. We're your
-                  creative partner. </p>
-                <div class="btn-box">
-                  <a href="#about" class="btn-1"> Learn more </a>
-                  <a href="#contact" class="btn-2">Get A Quote</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      </section>
     </div>
-    <div class="container idicator_container">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-    </div>
-  </div>
-</section>
-<!-- end slider section -->
-</div>
+  );
+};
+
+export default HeroArea;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
+import HeroArea from '../components/home/hero-section'
 
 export default function Index() {
   return (
       <Layout>
+        <HeroArea />
       </Layout>
   )
 }
