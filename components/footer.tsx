09a3dad00cfc3d813b5cb16e0e3ac1cc393bce71
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-11 15:25:39 +0300

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,27 +3,16 @@ import { EXAMPLE_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-blue-800 border-t border-neutral-200 pt-2 pb-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
+
+        <footer className="container-fluid footer_section">
+          <p className="text-white">
+            &copy; <span id="currentYear"></span> All Rights Reserved.
+            <a href="https://www.youtube.com/@SilusSledgeAmbani">
+              Sledge Entertainment LTD</a>
+          </p>
+        </footer>
       </Container>
     </footer>
   )
