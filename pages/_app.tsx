91c2bb451b770e980a31e572a372f0be8ac94107
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-11 17:50:30 +0300

    add css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import '../styles/custom.css'
-import 'font-awesome/css/font-awesome.min.css';
+import '../styles/font-awesome.min.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
