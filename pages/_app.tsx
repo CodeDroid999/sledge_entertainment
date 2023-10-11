c9ab07d9af8dca483cd337d297edf044aaa010cb
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-11 17:41:26 +0300

    add font-awesome.min.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import '../styles/custom.css'
 import 'font-awesome/css/font-awesome.min.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
