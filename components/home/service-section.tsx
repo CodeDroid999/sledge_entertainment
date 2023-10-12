81f9e30b8c2fd9b15fb68dde3d6e79a8198e9366
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-12 11:01:41 +0300

    add svg icon

--- components/home/service-section.tsx
+++ components/home/service-section.tsx
@@ -20,49 +20,61 @@ const ServiceSection: React.FC = () => {
              to life – from imagination to extraordinary reality."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="SCRIPT WRITING" />}
+            title="SCRIPT WRITING"
+            description="Scriptwriting is our forte at Sledge Entertainment Limited. We have a dedicated team of experienced
+             professionals who specialize in crafting engaging narratives, bringing your ideas to life with words that resonate on screen."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="FILM PRODUCTION" />}
+            title="PRODUCTION"
+            description="Production is where Sledge Entertainment Limited truly shines. With a team of seasoned professionals, 
+            we transform your vision into reality, executing a seamless and top-notch production process from inception to completion."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="CINEMATOGRAPHY" />}
+            title="CINEMATOGRAPHY"
+            description="Cinematography is our canvas at Sledge Entertainment Limited. Our skilled team of visual artists captures
+             your story with finesse, creating stunning visuals that elevate your project to new heights."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="GRAPHICS DESIGN" />}
+            title="GRAPHICS DESIGN"
+            description="Graphic design is a cornerstone of our creative arsenal. Our talented designers craft visuals that not
+             only captivate but also communicate your project's essence effectively. With an eye for aesthetics and a passion for innovation, we bring your ideas to life through compelling graphics."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="FINAL PACKAGING" />}
+            title="FINAL PACKAGING"
+            description="Final packaging is the culmination of our meticulous work at Sledge Entertainment Limited. We ensure your project is presented flawlessly, ready for its audience. 
+                        Our attention to detail in packaging guarantees a polished and professional presentation that leaves a lasting impression."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="PHOTOGRAPHY" />}
+            title="PHOTOGRAPHY"
+            description="ledge Entertainment Limited also offers expert photography services. Our experienced team excels in capturing 
+            the essence of your project with precision and creativity, delivering stunning visuals that enhance your storytelling."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="EDITINGe 2" />}
+            title="EDITING"
+            description="Editing is where Sledge Entertainment Limited truly transforms raw footage into cinematic brilliance. Our
+             skilled editing team assembles and refines your content, ensuring a polished and captivating final product that leaves
+              a lasting impression."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="COLOR GRADING" />}
+            title="COLOR GRADING"
+            description="Color grading is an art form at Sledge Entertainment Limited. Our expert colorists enhance the visual appeal
+             of your content, creating a mood and tone that aligns perfectly with your vision. With a keen eye for detail, we bring depth 
+             and vibrancy to your project."
           />
           <ServiceBox
-            svg={<img src="/images/service-icon-2.svg" alt="Service 2" />}
-            title="Service 2 Title"
-            description="Service 2 Description"
+            svg={<img src="/images/service-icon-2.svg" alt="AUDIO FINAL MIX" />}
+            title="AUDIO FINAL MIX"
+            description="Audio final mix is where Sledge Entertainment Limited perfects the auditory experience. Our dedicated sound 
+            engineers meticulously blend and refine the audio elements, ensuring that every sound, from dialogue to music, harmonizes 
+            seamlessly and immerses the audience in your project."
           />
         </div>
       </div>
