4bc10b5c87c1eda75ee7f2c2898438b68c608054
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-12 09:29:17 +0300

    add aboutsection to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 import HeroArea from '../components/home/hero-section'
 import Navbar from '../components/layout/navbar'
+import AboutSection from '../components/home/about-section'
 
 export default function Index() {
   return (
       <Layout>
         <HeroArea />
+        <AboutSection />
       </Layout>
   )
 }
