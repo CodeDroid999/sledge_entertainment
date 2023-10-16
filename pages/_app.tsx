75f673c9c88ff3cf03e2fb54e4545669d0108270
CodeDroid99 <johnmwega2002@gmail.com>
2023-10-16 17:23:05 +0300

    ADD SANITY

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,31 @@ import 'styles/responsive.css';
 import $ from "jquery"
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+import 'tailwindcss/tailwind.css'
+
+
+export interface SharedPageProps {
+  draftMode: boolean
+  token: string
+}
+
+const PreviewProvider = lazy(() => import('components/PreviewProvider'))
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<SharedPageProps>) {
+  const { draftMode, token } = pageProps
+  return (
+    <>
+      {draftMode ? (
+        <PreviewProvider token={token}>
+          <Component {...pageProps} />
+        </PreviewProvider>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  )
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,56 +1,45 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import IndexPage from 'components/IndexPage'
+import PreviewIndexPage from 'components/PreviewIndexPage'
+import { readToken } from 'lib/sanity.api'
+import { getAllPosts, getClient, getSettings } from 'lib/sanity.client'
+import { Post, Settings } from 'lib/sanity.queries'
+import { GetStaticProps } from 'next'
+import type { SharedPageProps } from 'pages/_app'
 
-type Props = {
-  allPosts: Post[]
+interface PageProps extends SharedPageProps {
+  posts: Post[]
+  settings: Settings
 }
 
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+interface Query {
+  [key: string]: string
 }
 
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
+export default function Page(props: PageProps) {
+  const { posts, settings, draftMode } = props
+
+  if (draftMode) {
+    return <PreviewIndexPage posts={posts} settings={settings} />
+  }
+
+  return <IndexPage posts={posts} settings={settings} />
+}
+
+export const getStaticProps: GetStaticProps<PageProps, Query> = async (ctx) => {
+  const { draftMode = false } = ctx
+  const client = getClient(draftMode ? { token: readToken } : undefined)
+
+  const [settings, posts = []] = await Promise.all([
+    getSettings(client),
+    getAllPosts(client),
   ])
 
   return {
-    props: { allPosts },
+    props: {
+      posts,
+      settings,
+      draftMode,
+      token: draftMode ? readToken : '',
+    },
   }
 }
